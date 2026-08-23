2fa6e202d74a541f4c019897e4d45f95bc4563ab
Afreen164 <afreensyeda2005@gmail.com>
2026-08-23 15:41:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (53.80%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n==0){
+
+            return 0;
+        }
+        else if(n==1){
+            return 1;
+        }
+        else if(n==2){
+            return 1;
+        }
+
+        int one=0;
+        int two =1;
+        int three=1;
+
+        for(int i=1; i<=n; i++){
+            int four= one + two + three;
+
+            one = two;
+            two = three;
+            three= four;
+        }
+        return one;
+        
+    }
+}
